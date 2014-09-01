1ec5b14a545c043b66cb1b435a3a60ef4dec3346
Alex Shurf Frenkel <sirshurf@yahoo.com>
2014-09-01 15:54:04 +0300

    added files

--- README.md
+++ README.md
@@ -1,4 +1,70 @@
-gars-zombie
+Google Analytics Real Time Scraping (using ZombieJs)
 ===========
 
-Node Google Analytics Realtime Scraper (using ZombieJs)
+Currently logs in to Google Analytics and scrapes the realtime page for the active visitors number from the overview screen.
+
+Notes: 
+- Inspired by: 
+ - <https://github.com/jedi4ever/gars> - This project is the reworked version of jedi4ever that I had to remove CasperJs.
+ - <https://github.com/adamdunkley/casperjs-google-analytics-realtime-scrape>
+ - <http://qzaidi.github.io/2013/06/23/dashboards-with-dashing/>
+- Currently only support datadog as a backend, more to come soon
+- This project uses zombiejs to scrape the metrics
+
+# Installation
+
+## Using the official npm
+`$ npm install -g gars-zombie`
+
+## From this repo
+```
+$ git clone https://github.com/sirshurf/gars-zombie.git
+$ cd gars-zombie
+$ npm install
+```
+
+# Configuration
+```
+    {
+      "google": {
+        "email": "<your email>",
+          "password": "<your password>",
+          "analytics": {
+            "home_id": "<your google analytics home id>",
+            "report_id": "<your google analytics project id>"
+          }
+      },
+      "casperjs": {
+        "verbose": true,
+        "logLevel": "info"
+      },
+      "metric": {
+        "name": "google.analytics.visitors",
+        "tags": [ "tag1", "tag2" ],
+        "hostname": "<your hostname>"
+      },
+      "backend": {
+          "datadog": {
+            "api_key": "<your datadog api key",
+            "url": "https://app.datadoghq.com/api/v1/series"
+          }
+        }
+    }
+```
+
+# Running it
+## From this repo
+`$ ./bin/gars-zombie -c <config file>`
+
+## From npm (global)
+`$ gars-zombie -c <config file>`
+
+## From npm local
+`$ ./node_modules/gars/bin/gars-zombie -c <config file>`
+
+# Finding the ids
+Once logged into the analytics. Note the following ids:
+
+- `google.analytics.home_id` : `https://www.google.com/analytics/web/?hl=en#home/<home_id>/`
+- `google.analytics.report_id` : `https://www.google.com/analytics/web/?hl=en#report/visitors-overview/<report_id>/`
+
---
+++ index.js
@@ -0,0 +1,139 @@
+var system = require('system');
+var fs = require('fs');
+var Browser = require("zombie");
+var request = require("request");
+
+var backend = function(metricArray, metric){
+	var backend = metricArray.backend;
+	var backendConfig = config.backend[backend];
+
+	switch(backendConfig.type) {
+		case 'datadog':
+			datadog(backendConfig,metricArray, metric);
+		break;
+		
+		default:
+		exit(-1);
+	} 
+
+}
+
+var datadog = function(backendConfig,metricArray, total){
+	// Send Data to DatadogHQ
+	var metricName = metricArray.name;
+	var metricTags = metricArray.tags;
+	var metricHostName = metricArray.hostname;
+
+	var datadogApiKey = backendConfig.api_key;
+	var datadogUrl = 'https://app.datadoghq.com/api/v1/series' || backendConfig.url;
+
+
+	var ts = Math.floor(new Date().getTime()/1000);
+
+	var metric = {
+		metric: metricName,
+		points:  [ [ ts , +total ] ],
+		host: metricHostName,
+		tags: metricTags,
+		type: 'gauge'
+	};
+
+	//console.log(JSON.stringify(metric));
+	request(
+		{ method: 'POST'
+		, uri: datadogUrl+'?api_key='+datadogApiKey
+		, json:true
+		, body: JSON.stringify({ 'series' : [metric] })
+		}
+		, function (error, response, body) {
+			if(response.statusCode == 202){
+				console.log('Accepted: '+ total)
+				//console.log(body)
+			} else {
+				console.log('error: '+ response.statusCode)
+				console.log(body)
+			}
+
+		}
+	)
+
+}
+
+var browserVisit = function (browser, configLine) {
+	var report = configLine.report;
+	report.forEach(function(reportLine) {
+		var analyticsHomeId = reportLine.analytics.home_id;
+		var analyticsReportId = reportLine.analytics.report_id;
+
+		browser.visit("https://www.google.com/analytics/realtime/getData?key="+analyticsHomeId+"&ds="+analyticsReportId+"&pageId=RealtimeReport%2Frt-overview&q=t%3A0|%3A1|%3A0%3A%2C&f", function(e) {
+			var StrippedString = browser.html('body').replace(/(<([^>]+)>)/ig,"");
+			var JsonParsed = JSON.parse(StrippedString);
+			var TotalMEtrics = JsonParsed["t:0|:1|:0:"]["metricTotals"][0];
+
+			//console.log("The page:", TotalMEtrics );
+
+			backend( reportLine.metric, TotalMEtrics);
+
+			// Infinite loop
+			setTimeout(function () {
+				browserVisit(browser, configLine);
+			    }, 1000);
+
+			});
+
+	});
+}
+
+/*
+ * Read configuration file
+ */
+var args = require('system').args;
+var configFile = args[2];
+var config = require(configFile);
+
+config.scraper.forEach(function(configLine) {
+
+	var googleEmail = configLine.google.email;
+	var googlePassword = configLine.google.password;
+	// Make Login
+	browser = new Browser();
+	browser.visit("https://accounts.google.com/ServiceLogin?service=analytics&passive=true&nui=1&hl=en&continue=https%3A%2F%2Fwww.google.com%2Fanalytics%2Fweb%2F%3Fhl%3Den&followup=https%3A%2F%2Fwww.google.com%2Fanalytics%2Fweb%2F%3Fhl%3Den", function () {
+		console.log('First Page');
+		// Fill email, password and submit form
+		browser.
+			fill("Email", googleEmail).
+			fill("Passwd", googlePassword).
+			pressButton("#signIn", function() {
+				browserVisit(browser, configLine);
+
+			});
+	});
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
